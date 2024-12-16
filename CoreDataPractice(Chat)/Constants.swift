fb14a9ccc38fd176aeed9df627272350e670c915
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-16 15:37:32 +0500

    Some constants work done

---
+++ CoreDataPractice(Chat)/Constants.swift
@@ -0,0 +1,21 @@
+//
+//  Constants.swift
+//  CoreDataPractice(Chat)
+//
+//  Created by Elexoft on 16/12/2024.
+//
+
+import Foundation
+
+struct K {
+    
+    struct Segues {
+        static let chatToMessageSegue = "ChatScreenToMessageScreen"
+    }
+    
+    struct Identifiers {
+        static let chatCellIdentifier = "ChatReuseableCell"
+        static let messageCellIdentifier = "MessageReuseableCell"
+    }
+    
+}
