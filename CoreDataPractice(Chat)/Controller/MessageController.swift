bdb2c67a466351599c76d36cd8d0db331be314d3
Muhammad Hasnain Bangash <bangashmhasnain38@gmail.com>
2024-12-16 15:40:38 +0500

    Message cell and chat cell label outlet work done

---
+++ CoreDataPractice(Chat)/Controller/MessageController.swift
@@ -0,0 +1,18 @@
+//
+//  MessageController.swift
+//  CoreDataPractice(Chat)
+//
+//  Created by Elexoft on 16/12/2024.
+//
+
+import UIKit
+
+class MessageController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
--- CoreDataPractice(Chat)/View/ChatCell.swift
+++ CoreDataPractice(Chat)/View/ChatCell.swift
@@ -9,6 +9,8 @@ import UIKit
 
 class ChatCell: UITableViewCell {
 
+    @IBOutlet weak var nameLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
